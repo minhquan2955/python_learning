836de6834101c16fc2ad2c118cdad589f8dd4273
minhquan2955 <minhquan2955@gmail.com>
2025-12-10 23:04:07 +0700

    dem tan suat

--- array_basic06.py
+++ array_basic06.py
@@ -1,16 +1,11 @@
-from collections import defaultdict
-from collections import Counter
-# Dùng Counter cho 99% trường hợp đếm tần suất.
-# Dùng defaultdict(int) nếu bạn cần tùy biến logic đếm phức tạp.
+
 n = int(input())
 numbers = [int(x) for x in input().split()]
 k = int(input())
-#ds = defaultdict(int)
-ds = Counter(numbers)
-#for x in numbers:
-    #ds[x] += 1
 cnt = 0
-for x in ds:
-    if k - x in ds:
-        cnt += max(ds[x],ds[k-x])
-print(cnt)        
+for i in range(0,n):
+    if numbers[i] <= k:
+        for j in range(i+1,n):
+            if numbers[j] + numbers[i] == k:
+                cnt += 1
+print(cnt)
